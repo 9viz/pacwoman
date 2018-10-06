4bd6429c39be800b25eb163ec625e648a1969eb0
The_Human <visuweshmuthukumar@gmail.com>
2018-10-06 23:33:27 +0530

    Improve readability by limiting max char per line to 80

--- pacwoman.py
+++ pacwoman.py
@@ -12,19 +12,20 @@ import sys
 import json
 from getpass import getuser
 
-#import files
+# import files
 import configuration
 import error_insults
 import search
 
-#get username
-if getuser() == "root" and configuration.root_execute == False:
-    print("{0}error:{1} this program is not allowed to be used as the root user".format(configuration.color_error, configuration.color_normal))
+# get username
+if getuser() == "root" and not configuration.root_execute:
+    print("{0}error:{1} this program is not allowed to be used as the root user"\
+            .format(configuration.color_error, configuration.color_normal))
 
-#get current directory
+# get current directory
 directory = os.getcwd()
 
-#change all colors to empty strings if colored_output is set to False in configuration.py
+# change all colors to empty strings if colored_output is set to False in configuration.py
 if not configuration.colored_output:
     configuration.color_normal = ""
     configuration.color_error = ""
@@ -32,38 +33,45 @@ if not configuration.colored_output:
     configuration.color_progress = ""
 
 def retrieve_file(package_name):
-#retrieves file from the AUR and saves it to the current working dir 
+# retrieves file from the AUR and saves it to the current working dir 
     url_package = "https://aur.archlinux.org/cgit/aur.git/snapshot/{}.tar.gz".format(package_name)
     tar_package = "{}.tar.gz".format(package_name)
     try:
         with urllib.request.urlopen(url_package) as response, open(tar_package, 'wb') as out_file:
             shutil.copyfileobj(response, out_file)
-        print ("{0}downloaded:{1} {2}.tar.gz has saved to {3}".format(configuration.color_successful, configuration.color_normal, package_name, directory))
+        print ("{0}downloaded:{1} {2}.tar.gz has saved to {3}"\
+                .format(configuration.color_successful, configuration.color_normal, package_name, directory))
     except urllib.error.HTTPError:
         if configuration.insults:
             error_insults.print_insult()
             sys.exit()
         else:
-            print ("{0}error:{1} target not found: {2}".format(configuration.color_error, configuration.color_normal, package_name))
+            print ("{0}error:{1} target not found: {2}"\
+                    .format(configuration.color_error, configuration.color_normal, package_name))
             sys.exit()
     except KeyboardInterrupt:
         sys.exit()
         
 def extract_tar(package_name):
-#extracts the downloaded tar and saves it in the cwd.
-#deletes the downloaded tar to prevent duplicates and confusion.
+# extracts the downloaded tar and saves it in the cwd.
+# deletes the downloaded tar to prevent duplicates and confusion.
     tar_package = "{}.tar.gz".format(package_name)
     tar = tarfile.open(tar_package, "r:gz")
-    print ("{0}extracting downloaded tarball.".format(configuration.color_progress))
+    print ("{0}extracting downloaded tarball."\
+            .format(configuration.color_progress))
     tar.extractall()
     tar.close()
-    print ("{0}extracted:{1} downloaded {2}.tar.gz has been extracted".format(configuration.color_successful, configuration.color_normal, package_name))
+    print ("{0}extracted:{1} downloaded {2}.tar.gz has been extracted"\
+            .format(configuration.color_successful, configuration.color_normal, package_name))
     try:
         subprocess.Popen("rm -rf {0}".format(tar_package), shell=True)
         wait()
-        print ("{0}removed: {1}tar package has been removed and the contents has been stored in {2}/{3}".format(configuration.color_progress, configuration.color_normal, directory, package_name))
+        print ("{0}removed: {1}tar package has been removed \
+                and the contents has been stored in {2}/{3}"\
+                .format(configuration.color_progress, configuration.color_normal, directory, package_name))
     except:
-        print ("{0}error:{1} can't remove downloaded tarball, please remove it manually").format(configuration.color_error, configuration.color_normal)
+        print ("{0}error:{1} can't remove downloaded tarball,\
+                please remove it manually").format(configuration.color_error, configuration.color_normal)
 
 def cd_to_package_dir():
     cd_to_dir = input("do you want to cd into the package directory? (y/n) ")
@@ -142,4 +150,5 @@ elif args.s:
 elif args.c:
     configuration.writeConfigDir(os.path.abspath(args.c))
 else:
-    print("{0}error:{1} no argument given. launch 'pacwoman -h' to know all the options available".format(configuration.color_error, configuration.color_normal))
+    print("{0}error:{1} no argument given. launch 'pacwoman -h' to know all the options available"\
+            .format(configuration.color_error, configuration.color_normal))
